b4bddf837fb9d279c01c3e96f78f4e0b2fc55d3f
TRMO <tromortensen@hotmail.com>
2025-03-13 12:53:05 +0100

    add comment

--- Shared/src/dtos/user/ViewUsers.java
+++ Shared/src/dtos/user/ViewUsers.java
@@ -1,5 +1,8 @@
 ﻿package dtos.user;
 
+// I have a wrapper abstract class, because the request and dto belong together.
+// This is a way to have them together in the same file.
+// If you plan on re-using the UserDto other places, maybe it should be moved to its own file.
 public abstract class ViewUsers {
     public record Request(int pageIndex, int pageSize, String firstNameContains){}
     public record UserDto(String email, String firstName, String lastName){}
