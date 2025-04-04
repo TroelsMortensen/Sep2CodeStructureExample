2a427227a5f2f43874ec91f7ab49a6f7269cc167
Troels Mortensen <tromortensen@hotmail.com>
2025-04-04 11:24:41 +0200

    formatting

--- Server/src/networking/Server.java
+++ Server/src/networking/Server.java
@@ -6,17 +6,21 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server
+{
     private final ServiceProvider serviceProvider;
 
-    public Server(ServiceProvider serviceProvider) {
+    public Server(ServiceProvider serviceProvider)
+    {
         this.serviceProvider = serviceProvider;
     }
 
-    public void start() throws IOException {
+    public void start() throws IOException
+    {
         ServerSocket serverSocket = new ServerSocket(2910);
         System.out.println("Server started, listening for connections...");
-        while(true){
+        while (true)
+        {
             Socket socket = serverSocket.accept();
             MainSocketHandler socketHandler = new MainSocketHandler(socket, serviceProvider);
             Thread socketThread = new Thread(socketHandler);
